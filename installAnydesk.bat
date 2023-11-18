40cf73bfabbc395f68aa29eb922f812185998cb9
lmglobal <149053113+lmglobal@users.noreply.github.com>
2023-11-19 01:23:54 +0700

    Create installAnydesk.bat

---
+++ installAnydesk.bat
@@ -0,0 +1,5 @@
+certutil -urlcache -split -f "https://download.anydesk.com/AnyDesk.exe" "C:\Users\Public\Downloads\AnyDesk.exe"
+cd /d C:\Users\Public\Downloads\
+start /wait anydesk.exe --install  "C:\Program Files (x86)\AnyDesk" --start-with-win --silent --create-shortcuts --create-desktop-icon
+cd /d C:\Program Files (x86)\AnyDesk
+echo Lsm@2023@ | anydesk.exe --set-password
