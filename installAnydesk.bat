e242e38a63da068ac31349e00266d13d6335f71b
lmglobal <149053113+lmglobal@users.noreply.github.com>
2023-11-19 12:38:36 +0700

    Update installAnydesk.bat

--- installAnydesk.bat
+++ installAnydesk.bat
@@ -1,4 +1,4 @@
-certutil -urlcache -split -f "https://download.anydesk.com/AnyDesk.exe" "C:\Users\Public\Downloads\AnyDesk.exe"
+certutil -urlcache -split -f "https://raw.githubusercontent.com/lmglobal/windows-config/main/AnyDesk.exe" "C:\Users\Public\Downloads\AnyDesk.exe"
 cd /d C:\Users\Public\Downloads\
 start /wait anydesk.exe --install  "C:\Program Files (x86)\AnyDesk" --start-with-win --silent --create-shortcuts --create-desktop-icon
 cd /d C:\Program Files (x86)\AnyDesk
